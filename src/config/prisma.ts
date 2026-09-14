34bd8b618b2ea872e834d1f0675600592f0a5439
Rabby-khan-04 <ajrabbyk72@gmail.com>
2026-09-15 00:07:46 +0600

    feat(db schema): Designed database schema

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from "../../generated/prisma/client";
 import { env } from "./env";
 
 const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter }).$extends({});
 
 export { prisma };
