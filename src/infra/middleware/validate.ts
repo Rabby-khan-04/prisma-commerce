2d8d706e93ec7d38b200eb903c707a3d28f89e1d
Rabby-khan-04 <ajrabbyk72@gmail.com>
2026-09-17 21:10:03 +0600

    feat(validator): Added reusable zod validator

--- src/infra/middleware/validate.ts
+++ src/infra/middleware/validate.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { z, ZodError } from "zod";
+import { ValidationError } from "../errors/app-error";
 
 export type ValidationTarget = "body" | "query" | "params";
 
@@ -34,11 +35,42 @@ export const validate = <T extends z.ZodType>(
     } catch (error) {
       if (error instanceof ZodError) {
         const errs = formatZodErrors(error);
+        next(new ValidationError("Validation failed", errs));
       }
 
       next(error);
     }
   };
 };
+
+export const commonSchema = z.object({
+  uuid: z.uuid("Invalid UUID"),
+
+  pagination: z.object({
+    page: z.coerce.number().int().positive().default(1),
+    limit: z.coerce.number().int().positive().max(100).default(10),
+  }),
+
+  email: z.email("Invalid email address").toLowerCase().trim(),
+
+  password: z
+    .string()
+    .min(8, "Password must be 8 characters")
+    .regex(/[a-zA-Z]/, "Password must contain at least one letter")
+    .regex(/[0-9]/, "Password mut contain at least one number"),
+
+  nonEmptyString: z.string().min(1, "This field cannot be empty").trim(),
+
+  noNegativeNumber: z.coerce.number().nonnegative("Must be zero or positive"),
+
+  booleanString: z
+    .union([z.boolean(), z.string()])
+    .transform((val) => val === true || val === "true"),
+
+  slug: z
+    .string()
+    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, "Invalid slug format")
+    .toLowerCase(),
+});
 
 export { z };
